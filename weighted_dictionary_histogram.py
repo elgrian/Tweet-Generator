dbfba7342464320bc61277f083df1e7ced6e5aa1
elgrian <anwar.loves.lacroix@gmail.com>
2018-11-05 23:05:26 -0800

    Added random_word function

--- weighted_dictionary_histogram.py
+++ weighted_dictionary_histogram.py
@@ -12,3 +12,19 @@ def histogram(text):
             
     return histogram_dictionary
 
+
+def random_word(histogram_dictionary):
+    '''Takes a Dictionary as a histogram and returns a random key from it'''
+    #Creates a running total value
+    accum = 0
+    #Gets a random number between 0 and the total sum of all frequencies
+    sum_dictionary = sum(histogram_dictionary.values())
+    random_sum = random.randint(0, sum_dictionary - 1) # Either [1,  sum] or [0, sum - 1] otherwise it repeats inappropraitely
+    # ".items()" allows the dictionary to be iterated over
+    for key, value in histogram_dictionary.items():
+        accum += value
+        if accum > random_sum:
+            return key
+        else:
+            continue
+            
