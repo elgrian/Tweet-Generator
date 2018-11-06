fcfa913ce23a2ca5447068f100f9c9024adb8eb8
elgrian <anwar.loves.lacroix@gmail.com>
2018-11-05 23:47:55 -0800

    I give up for the night

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask
 from flask import request
 app = Flask(__name__)
-import weighted_dictionary_histogram
+import weighted_dictionary_histogram as wdh
 
 
 @app.route('/')
 def get_tweet():
-    total_word = int(request.args.get('words'))
+    total_word = request.args.get('words')
     complete_string = ""
-    words = weighted_dictionary_histogram.file_open()
-    new_histogram = weighted_dictionary_histogram.histogram(text)
+    words = wdh.file_open()
+    histo = wdh.histogram(text)
     
-    for _ in range(total_word):
-        chosen_random_word = weighted_dictionary_histogram.random_word(new_histogram)
+    for _ in range(int(total_word)):
+        chosen_random_word = wdh.random_word(histo)
         complete_string = complete_string + " " + chosen_random_word
     return complete_string
+    
+# get_string()
+
+# if __name__ == '__main__':
+#     app.debug = True
+#     app.run()
